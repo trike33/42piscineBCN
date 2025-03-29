db3328a22babff73d04beeedfebf42c98e889266
trike33 <137085374+trike33@users.noreply.github.com>
2025-03-29 20:20:07 +0100

    Update ft_ultimate_ft.c

--- C01/ex01/ft_ultimate_ft.c
+++ C01/ex01/ft_ultimate_ft.c
@@ -3,11 +3,11 @@
 void  ft_ultimate_ft(int *********nbr)
 {
   *********nbr = 42;
-  char c1 = (*********nbr / 10) + '0';
+  /*char c1 = (*********nbr / 10) + '0';
   char c2 = (*********nbr % 10) + '0';
   write(1, &c1, 1);
   write(1, &c2, 1);
-  write(1, "\n", 1);
+  write(1, "\n", 1);*/
 }
 
 int  main(void)

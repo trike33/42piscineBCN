88aa7e7de5ee9c77fa18d118aeb4ab90320374c4
trike33 <137085374+trike33@users.noreply.github.com>
2025-03-30 16:03:45 +0200

    Update ft_ultimate_ft.c

--- C01/ex01/ft_ultimate_ft.c
+++ C01/ex01/ft_ultimate_ft.c
@@ -1,28 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfontbon <jfontbon@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/30 15:38:05 by jfontbon          #+#    #+#             */
+/*   Updated: 2025/03/30 15:45:21 by jfontbon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void  ft_ultimate_ft(int *********nbr)
+void	ft_ultimate_ft(int *********nbr)
 {
-  *********nbr = 42;
-  /*char c1 = (*********nbr / 10) + '0';
-  char c2 = (*********nbr % 10) + '0';
-  write(1, &c1, 1);
-  write(1, &c2, 1);
-  write(1, "\n", 1);*/
+	char	c1;
+	char	c2;
+
+	*********nbr = 42;
+	c1 = (*********nbr / 10) + '0';
+	c2 = (*********nbr % 10) + '0';
 }
 
-int  main(void)
+int	main(void)
 {
-  int i = 0;
-  int *ptr1 = &i;
-  int **ptr2 = &ptr1;
-  int ***ptr3 = &ptr2;
-  int ****ptr4 = &ptr3;
-  int *****ptr5 = &ptr4;
-  int ******ptr6 = &ptr5;
-  int *******ptr7 = &ptr6;
-  int ********ptr8 = &ptr7;
-  int *********ptr9 = &ptr8;
+	/*int	i;
+	int	*ptr1;
+	int	**ptr2;
+	int	***ptr3;
+	int	****ptr4;
+	int	*****ptr5;
+	int	******ptr6;
+	int	*******ptr7;
+	int	********ptr8;
+	int	*********ptr9;
 
-  ft_ultimate_ft(ptr9);
-  return (0);
+	i = 0;
+	*ptr1 = &i;
+	**ptr2 = &ptr1;
+	***ptr3 = &ptr2;
+	****ptr4 = &ptr3;
+	*****ptr5 = &ptr4;
+	******ptr6 = &ptr5;
+	*******ptr7 = &ptr6;
+	********ptr8 = &ptr7;
+	*********ptr9 = &ptr8;
+	ft_ultimate_ft(ptr9);*/
+	return (0);
 }

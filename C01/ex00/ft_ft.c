5e74a90b6deb30acf43141d5561c7ab3a26a2d0d
trike33 <137085374+trike33@users.noreply.github.com>
2025-03-30 16:03:18 +0200

    Update ft_ft.c

--- C01/ex00/ft_ft.c
+++ C01/ex00/ft_ft.c
@@ -1,20 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfontbon <jfontbon@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/30 15:31:19 by jfontbon          #+#    #+#             */
+/*   Updated: 2025/03/30 15:37:41 by jfontbon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include <unistd.h>
 
 void	ft_ft(int *nbr)
 {
-	*nbr = 42;
+	char	c1;
+	char	c2;
 
-	char c1 = (*nbr / 10) + '0';
-	char c2 = (*nbr % 10) + '0';
-	//write(1, &c1, 1);
-	//write(1, &c2, 1);
-	//write(1, "\n", 1);
+	*nbr = 42;
+	c1 = (*nbr / 10) + '0';
+	c2 = (*nbr % 10) + '0';
 }
 
 int	main(void)
 {
-	int i = 0;
-	int *ptr = &i;
+	int	i;
+	int	*ptr;
+
+	i = 0;
+	*ptr = &i;
 	ft_ft(ptr);
-	return(0);
+	return (0);
 }

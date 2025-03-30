e6e2673136d9596b3b2d4612aeff032fb148ead3
trike33 <137085374+trike33@users.noreply.github.com>
2025-03-30 16:04:20 +0200

    Update ft_swap.c

--- C01/ex02/ft_swap.c
+++ C01/ex02/ft_swap.c
@@ -1,24 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfontbon <jfontbon@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/30 15:45:45 by jfontbon          #+#    #+#             */
+/*   Updated: 2025/03/30 15:50:22 by jfontbon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_swap(int *a, int *b)
+void	ft_swap(int *a, int *b)
 {
-	int temp = *a;
+	int	temp;
+	//char	c1;
+	//char	c2;
+	temp = *a;
 	*a = *b;
 	*b = temp;
-	char c1 = (*a % 10) + '0';
-	char c2 = (*b % 10) + '0';
+	/*c1 = (*a % 10) + '0';
+	c2 = (*b % 10) + '0';
 	write(1, "a=", 2);
 	write(1, &c1, 1);
 	write(1, "b=", 2);
 	write(1, &c2, 1);
-	write(1, "\n", 1);
+	write(1, "\n", 1);*/
 }
 
-int main(void) {
-	int a = 0;
-	int b = 1;
-	int *ptrA = &a;
-	int *ptrB = &b;
-	ft_swap(ptrA, ptrB);
+int	main(void)
+{
+	int	a;
+	int	b;
+	int	*ptra;
+	int	*ptrb;
+
+	a = 0;
+	b = 1;
+	*ptrA = &a;
+	*ptrB = &b;
+	ft_swap(ptra, ptrb);
 	return (0);
 }

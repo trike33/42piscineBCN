c01151ec90e3f7ec4129cd35969f302270ced2f7
trike33 <137085374+trike33@users.noreply.github.com>
2025-03-31 19:50:01 +0200

    Create ft_strlcat.c

---
+++ C03/ex05/ft_strlcat.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfontbon <jfontbon@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/31 18:56:58 by jfontbon          #+#    #+#             */
+/*   Updated: 2025/03/31 19:44:56 by jfontbon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	int		i;	
+
+	i = 0;
+	while (*dest != '\0')
+	{
+		dest++;
+		i++;
+	}
+	while (*src != '\0' && i < size - 1)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		i++;
+	}
+	if (i == size - 1)
+	{
+		dest++;
+		*dest = '\0';
+	}
+	return (size);
+}
+
+/*int		main(void)
+{
+	char	dest[10] = "Hello";
+	char	src[] = "World!";
+	int		size = 10;
+	int		result = ft_strlcat(dest, src, size);
+	printf("%s\n", dest);
+	printf("%i\n", result);
+	return (0);
+}*/

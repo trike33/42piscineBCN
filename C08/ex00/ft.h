7e2e9c91dc4c7e5dd2b99bd9b30a2fa653e68572
Joan Fontbona Ardanaz <jfontbon@car7s1.42barcelona.com>
2025-04-06 18:00:57 +0200

    yes

---
+++ C08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfontbon <jfontbon@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/06 11:25:06 by jfontbon          #+#    #+#             */
+/*   Updated: 2025/04/06 11:26:10 by jfontbon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
